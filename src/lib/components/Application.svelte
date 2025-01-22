bd668e198b5206e052cbc2a1ad40c101866c9d01
DarthKadik <v.kadosa@gmail.com>
2025-01-22 15:42:55 +0200

    changed address

--- src/lib/components/Application.svelte
+++ src/lib/components/Application.svelte
@@ -78,7 +78,7 @@
       const baseURL = "";
 
 
-      const res = await fetch(`http://34.136.198.157:1337/api/form-responses`, {
+      const res = await fetch(`https://34.136.198.157/api/form-responses`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
